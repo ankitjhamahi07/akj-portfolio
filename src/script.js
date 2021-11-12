bedf32c2b4663b0b9623951b63a99537674ee958
Ankit <ankitjhamahi.07@gmail.com>
2021-11-12 21:30:59 +0530

    background-color

--- src/script.js
+++ src/script.js
@@ -1,3 +1,16 @@
+
+
+function checkDarkMode() {
+var body=document.body;
+
+if( document.body.className.contains('dark-mode') ) {
+      body.style.removeProperty("background");
+      body.style.removeProperty("animation");
+      body.style.backgroundColor="#121212";
+}
+}
+
+
 var form = document.getElementById("contact-form");
     
     async function handleSubmit(event) {
@@ -28,6 +41,13 @@ function switchToDarkMode()  {
   let name=document.getElementById("visitor-name");
   let message=document.getElementById("visitor-message");
   body.classList.toggle("dark-mode");
+  if(body.classList.contains("dark-mode")) {
+      body.removeAttribute("animation"); 
+      body.style.background="#121212";
+  } else {
+      body.removeAttribute("style");
+      body.setAttribute("background", "background: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);")
+  }
   email.classList.toggle("dark-mode-input");
   name.classList.toggle("dark-mode-input");
   message.classList.toggle("dark-mode-input");
